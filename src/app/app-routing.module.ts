07cb5cfd4af078142ce8e2a610df7e6156e7baff
carlos.gomes <carlos.gomes@castgroup.com.br>
2022-07-27 13:46:32 -0300

    Primeng

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const loginModule = () => import('./modules/login/login.module').then(m => m.LoginModule);
+
+const routes: Routes = [
+  { path: 'login', loadChildren: loginModule },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
